f25ce2c38daabd54c1c9e4c85069f54cd72f029e
wangyichen <ethan.wake@gmail.com>
2018-07-09 18:55:34 +0800

    10-3-6 custom SimpleCursorAdapter show name without phone number

--- app/src/main/java/com/eth/contentapp/MainActivity.java
+++ app/src/main/java/com/eth/contentapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.eth.contentapp;
 
 import android.Manifest;
 import android.content.ContentResolver;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.provider.ContactsContract;
@@ -11,10 +12,12 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.provider.ContactsContract.CommonDataKinds.Phone;
 import android.provider.ContactsContract.Contacts;
+import android.widget.TextView;
 
 import static android.Manifest.permission.*;
 
@@ -65,14 +68,11 @@ public class MainActivity extends AppCompatActivity {
 
     private void readContacts() {
         ContentResolver resolver = getContentResolver();
-        String[] projecton = {Contacts._ID, Contacts.DISPLAY_NAME, Phone.NUMBER};
-        Cursor cursor = resolver.query(
-                Phone.CONTENT_URI,
-                projecton,
+        Cursor cursor = resolver.query(Contacts.CONTENT_URI,
                 null,
                 null,
-                null
-        );
+                null,
+                null);
 
 //        while (cursor.moveToNext()) {
 //            //處理每一筆資料
@@ -82,15 +82,40 @@ public class MainActivity extends AppCompatActivity {
 //                    ContactsContract.Contacts.DISPLAY_NAME));
 //            Log.d("RECORD", "readContacts: "+id+"/"+name);
 //        }
-
-        ListView list = findViewById(R.id.list);
-        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(
+                this,
                 android.R.layout.simple_list_item_2,
                 cursor,
-                new String[]{ContactsContract.Contacts.DISPLAY_NAME,
-                Phone.NUMBER},
-                new int[]{android.R.id.text1,android.R.id.text2},
-                1);
+                new String[]{Contacts.DISPLAY_NAME,
+                        Contacts.HAS_PHONE_NUMBER},
+                new int[]{android.R.id.text1,
+                        android.R.id.text2},
+                1) {
+            //客製化
+            @Override
+            public void bindView(View view, Context context, Cursor cursor) {
+                super.bindView(view, context, cursor);
+                TextView phone = view.findViewById(android.R.id.text2);
+                if (cursor.getInt(cursor.getColumnIndex(
+                        ContactsContract.Contacts.HAS_PHONE_NUMBER)) == 0) {
+                    phone.setText("");
+                } else {
+                    int id = cursor.getInt(cursor.getColumnIndex(Contacts._ID));
+                    Cursor pCursor = getContentResolver().query(
+                            Phone.CONTENT_URI,
+                            null,
+                            Phone.CONTACT_ID+"=?",
+                            new String[]{String.valueOf(id)},
+                            null);
+                    if (pCursor.moveToFirst()) {
+                        String number = pCursor.getString(pCursor.getColumnIndex(
+                                Phone.DATA));
+                        phone.setText(number);
+                    }
+                }
+            }
+        };
+        ListView list = findViewById(R.id.list);
         list.setAdapter(adapter);
     }
 }
